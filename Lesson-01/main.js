fabbb7dbff16d0ff15eced7164307037dfaa11cc
taras.poli <taras.poli@gmail.com>
2020-01-10 10:18:13 +0200

    Final changes

--- Lesson-01/main.js
+++ Lesson-01/main.js
@@ -15,50 +15,66 @@ let primary = 100,
 // Task-01
 // Из двух чисел с разной четностью вывести на экран нечетное число.
 let result1;
-if (primary < 0) {
-    result1 = primary
-} else if (secondary < 0) {
-    result1 = secondary
-} else {
-    result1 = "All numbers > 0"
-}
+
+    if (primary < 0) {
+        result1 = primary
+    } else if (secondary < 0) {
+        result1 = secondary
+    } else {
+        result1 = "All numbers > 0"
+    }
+
 console.log('Negative number:', result1);
 
 // Task-02
 // Определить количество дней в году, который вводит пользователь.
+let year = 2020,
+    tempVar1 = year%4,
+    tempVar2 = year%400;
+
+    if ( tempVar1!=0 && tempVar1!=0) {
+        result2 = 'NORMAL'
+    } else {
+        result2 = "LEAP"
+    }
+    
+console.log(year + ' is a ' + result2 + ' year');
 
 // Task-03
 // Вводятся три разных числа. Найти, какое из них является средним.
 let first = 3,
     second = 9,
     third = 5,
-    result2;
+    result3;
 
-if ((second<first && first<third) || (third<first && first<second)) {
-    result2 = first
-} else if ((first<second && second<third) || (third<second && second<first)) {
-    result2 = second
-} else if ((first<third && third<second) || (second<third && third<first)) {
-    result2 = third
-} else {
-    result2 = "Similar numbers found"
-}
-console.log('Average:', result2);
-
-// Task-04
-// Вводятся три целых числа. Определить какое из них наибольшее.
-let result3;
-    // result3 = Math.max.apply(null, [1,2,3,-1,10,-2,0]);
-    if (first>second && first>third){
+    if ((second<first && first<third) || (third<first && first<second)) {
         result3 = first
-    } else if (second>first && second>third){
+    } else if ((first<second && second<third) || (third<second && second<first)) {
         result3 = second
-    } else if (third>first && third>second){
+    } else if ((first<third && third<second) || (second<third && third<first)) {
         result3 = third
     } else {
         result3 = "Similar numbers found"
     }
+
 console.log('Average:', result3);
+
+// Task-04
+// Вводятся три целых числа. Определить какое из них наибольшее.
+let result4;
+
+    // result4 = Math.max.apply(null, [1,2,3,-1,10,-2,0]);
+    if (first>second && first>third){
+        result4 = first
+    } else if (second>first && second>third){
+        result4 = second
+    } else if (third>first && third>second){
+        result4 = third
+    } else {
+        result4 = "Similar numbers found"
+    }
+
+console.log('Average:', result4);
 
 // Task-05
 // Закомменировать 1 строчку однострочным комментарием и остальные многострочным
